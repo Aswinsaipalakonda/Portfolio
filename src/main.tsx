0f20e6ea8623175ab391bcdb99e498b8b9604cb8
Aswinsaipalakonda <aswinsaipalakonda@gmail.com>
2026-02-21 05:36:20 +0530

    vercel analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { LenisProvider } from './providers/LenisProvider';
 import './index.css';
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LenisProvider>
+      <Analytics />
       <App />
     </LenisProvider>
   </StrictMode>
